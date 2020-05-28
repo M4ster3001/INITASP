93085dfcb221ddd62ddbe3946dc6220bb52ec5b8
Aldo Junior <aldocfjunior@gmail.com>
2020-05-28 12:17:33 -0300

    começando views

--- CRUD/Controllers/UsuarioController.cs
+++ CRUD/Controllers/UsuarioController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CRUD.Models.Contexto;
+using CRUD.Models.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CRUD.Controllers
 {
@@ -16,7 +18,121 @@ namespace CRUD.Controllers
 
         public IActionResult Index()
         {
-            return View();
+
+            var lista = _contexto.Usuario.ToList();
+
+            return View(lista);
+
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+
+            var usuario = new Usuario();
+            CarregarTipoUsuario();
+
+            return View(usuario);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario usuario) 
+        {
+
+            if( ModelState.IsValid ) 
+            {
+                _contexto.Usuario.Add(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Index");   
+            }
+
+            CarregarTipoUsuario();
+            return View(usuario);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            var usuario = _contexto.Usuario.Find(id);
+
+            CarregarTipoUsuario();
+            if (usuario != null)
+            {
+                return View(usuario);
+            }
+
+            return View("Usuário não encontrado");
+        }
+
+        [HttpPost]
+        public IActionResult Edit( Usuario usuario )
+        {
+
+            if (ModelState.IsValid)
+            {
+                _contexto.Usuario.Update(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            CarregarTipoUsuario();
+            return View(usuario);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+
+            var usuario = _contexto.Usuario.Find(id);
+
+            CarregarTipoUsuario();
+            return View(usuario);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Usuario _usuario)
+        {
+
+            var usuario = _contexto.Usuario.Find(_usuario.id);
+            if (usuario != null)
+            {
+                _contexto.Usuario.Remove(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+
+            var usuario = _contexto.Usuario.Find(id);
+
+            CarregarTipoUsuario();
+            return View(usuario);
+        }
+
+        public void CarregarTipoUsuario()
+        {
+
+            var ItensTipoUsuario = new List<SelectListItem>
+            {
+                new SelectListItem{ Value="1", Text="Administrador" },
+                new SelectListItem{ Value="2", Text="Técnico" },
+                new SelectListItem{ Value="3", Text="Normal" },
+            };
+
+            ViewBag.TiposUsuario = ItensTipoUsuario;
+
         }
     }
 }
